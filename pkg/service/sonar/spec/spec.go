735ccc70183556b367cbff50b445111d6f52bfce
Alexander Morozov <alexander_morozov@epam.com>
2019-09-24 20:24:41 +0300

    Decrease delay time in readiness probes
    Change-Id: I043d66a09fba918b2ad77dbb8c4aaffe5b4bda05

--- pkg/service/sonar/spec/spec.go
+++ pkg/service/sonar/spec/spec.go
@@ -6,9 +6,9 @@ const (
 	DbImage                           = "postgres:9.6"
 	DBPort                            = 5432
 	LivenessProbeDelay                = 180
-	ReadinessProbeDelay               = 180
+	ReadinessProbeDelay               = 60
 	DbLivenessProbeDelay              = 180
-	DbReadinessProbeDelay             = 180
+	DbReadinessProbeDelay             = 60
 	MemoryRequest                     = "500Mi"
 	JenkinsPluginConfigPostfix string = "jenkins-plugin-config"
 )
